6500a3243324530f71676bde7fbcef885f5c425d
Jhordan <jhordanX@gmail.com>
2022-06-14 00:40:33 -0300

    Learning Java
    Herança e Polimorfismo, assunto um pouco complicado.

---
+++ src/Inheritance/Main.java
@@ -0,0 +1,13 @@
+package Inheritance;
+
+import Inheritance.Entities.BusinessAccount;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BusinessAccount account = new BusinessAccount();
+
+        account.getBalance();
+
+    }
+}
